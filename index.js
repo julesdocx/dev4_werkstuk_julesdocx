3e0c4a01ea5432b9b19791dc6a0e2830162194d9
docxjules <jules.docx@gmail.com>
2020-08-28 19:32:03 +0200

    count function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var allData; //imported json
 
-var podia = [HTMLElement]; //HTMLElements
-
+var doelGroepFilters
 
 fetch('/entries.json')
   .then(res => res.json())
@@ -11,20 +10,30 @@ fetch('/entries.json')
 	theBigDevFunction();
 });
 
+
+const count = (arr, classifier) => {
+    return arr.reduce(function(counter, item) {
+        var p = (classifier || String)(item);
+        counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+        return counter;
+    }, {})
+}
+
+
+const filter = (filterName, filterStatus) => {
+	
+}
+
 const theBigDevFunction = () => {
-	let containerFilter = document.createElement('DIV')
-	containerFilter.setAttribute('class', 'container')
-	containerFilter.setAttribute('id', 'filter-container')
-	document.body.appendChild(containerFilter)
-	let containerPodia = document.createElement('DIV')
-	containerPodia.setAttribute('class', 'container')
-	containerPodia.setAttribute('id', 'podia-container')
-	document.body.appendChild(containerPodia)
 	createHTMLElementsForEveryPodia()
 	createHTMLElementsForEveryFilter()
 }
 
 const createHTMLElementsForEveryPodia = () => {
+	let containerPodia = document.createElement('DIV')
+	containerPodia.setAttribute('class', 'container')
+	containerPodia.setAttribute('id', 'podia-container')
+	document.body.appendChild(containerPodia)
 	for (const podium of allData) {
 		let element = document.createElement('DIV')
 		element.append(`${podium.name}`)
@@ -34,5 +43,19 @@ const createHTMLElementsForEveryPodia = () => {
 	}
 }
 const createHTMLElementsForEveryFilter = () => {
+	let containerFilter = document.createElement('DIV')
+	containerFilter.setAttribute('class', 'container')
+	containerFilter.setAttribute('id', 'filter-container')
+	document.body.appendChild(containerFilter)
+	
+	// var numVolwassenen = allData.reduce(function(n, genre) {
+	// 	return n + (genre.genre == 'theater');
+	// }, 0);
+	
+	// var numFamilie = allData.reduce(function(n, genre) {
+	// 	return n + (genre.category == 'familie');
+	// }, 0);
 
+    console.log(count(allData, function(item) { return item.genre })
+	)
 }
