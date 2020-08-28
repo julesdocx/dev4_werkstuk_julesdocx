6bb9939a4645fa98289b25e5e6ad2e5980556c2d
docxjules <jules.docx@gmail.com>
2020-08-29 00:54:31 +0200

    filters

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var allData; //imported json
+var arr;
 
-var doelGroepFilters
+document.getElementById('filterVolwassenen').onclick( (button) => {
+	event('volwassenen') 
+	button.color('green')})
+document.getElementById('filterVolwassenen').onclick( (button) => event('volwassenen'))
 
 fetch('/entries.json')
   .then(res => res.json())
   .then((json) => {
-	console.log(`Got the response, there are ${json.items.length} entries`)
-	allData = json.items;
-	theBigDevFunction();
+	console.log(`Got the response, there are ${json.items.length} entries`);
+	arr = [...json.items]
+	print(arr);
 });
 
 
-const count = (arr, classifier) => {
+
+const existfilter = item => item == !null
+const itemToLowerCase = item => item['genre-v2'].toLowerCase()
+const trimGenre = item => item['genre-v2'].trim()
+
+const count = (arr, classifier) => { //count(allData, function(item) { return item['genre-v2']}))
     return arr.reduce(function(counter, item) {
         var p = (classifier || String)(item);
         counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
@@ -19,34 +27,42 @@ const count = (arr, classifier) => {
     }, {})
 }
 
-
-const filter = (filterName, filterStatus) => {
-	
-}
-
-const theBigDevFunction = () => {
-	createHTMLElementsForEveryPodia()
-	createHTMLElementsForEveryFilter()
-}
-
-const createHTMLElementsForEveryPodia = () => {
-	let containerPodia = document.createElement('DIV')
-	containerPodia.setAttribute('class', 'container')
-	containerPodia.setAttribute('id', 'podia-container')
-	document.body.appendChild(containerPodia)
-	for (const podium of allData) {
-		let element = document.createElement('DIV')
-		element.append(`${podium.name}`)
-		element.setAttribute('class', 'content-card')
-		element.setAttribute('id', podium.name)
-		document.getElementById('podia-container').appendChild(element)
+const event = ( filterName) => {
+	if (filterName == 'volwassenen') {
+		const arrFiltered = [...arr.filter((value) => { return value.category == filterName })]
+		console.log(arrFiltered)
+	} else if (filterName == 'familie') {
+		const arrFiltered = [...arr.filter((value) => { return value.category == filterName })]
+		console.log(arrFiltered)
+	} else {
+		const dataCopy = [...arr];
 	}
 }
-const createHTMLElementsForEveryFilter = () => {
-	let containerFilter = document.createElement('DIV')
-	containerFilter.setAttribute('class', 'container')
-	containerFilter.setAttribute('id', 'filter-container')
-	document.body.appendChild(containerFilter)
+
+
+// const theBigDevFunction = () => {
+// 	createHTMLElementsForEveryPodia()
+// 	createHTMLElementsForEveryFilter()
+// }
+
+// const createHTMLElementsForEveryPodia = () => {
+// 	let containerPodia = document.createElement('DIV')
+// 	containerPodia.setAttribute('class', 'container')
+// 	containerPodia.setAttribute('id', 'podia-container')
+// 	document.body.appendChild(containerPodia)
+// 	for (const podium of allData) {
+// 		let element = document.createElement('DIV')
+// 		element.append(`${podium.name}`)
+// 		element.setAttribute('class', 'content-card')
+// 		element.setAttribute('id', podium.name)
+// 		document.getElementById('podia-container').appendChild(element)
+// 	}
+// }
+// const createHTMLElementsForEveryFilter = () => {
+// 	let containerFilter = document.createElement('DIV')
+// 	containerFilter.setAttribute('class', 'container')
+// 	containerFilter.setAttribute('id', 'filter-container')
+// 	document.body.appendChild(containerFilter)
 	
 	// var numVolwassenen = allData.reduce(function(n, genre) {
 	// 	return n + (genre.genre == 'theater');
@@ -56,6 +72,5 @@ const createHTMLElementsForEveryFilter = () => {
 	// 	return n + (genre.category == 'familie');
 	// }, 0);
 
-    console.log(count(allData, function(item) { return item.genre })
-	)
-}
+//     console.log(count(allData, function(item) { return item.genre }))
+// }
