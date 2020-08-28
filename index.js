46177f29907c445650d6364bb46c4290075d420d
docxjules <jules.docx@gmail.com>
2020-08-28 16:31:05 +0200

    loading in the elements in a grid

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-var allData;
+var allData; //imported json
+
+var podia = [HTMLElement]; //HTMLElements
+
 
 fetch('/entries.json')
   .then(res => res.json())
   .then((json) => {
 	console.log(`Got the response, there are ${json.items.length} entries`)
-	allData = JSON.stringify(json.items);
-	printAlldata();
+	allData = json.items;
+	theBigDevFunction();
 });
 
-const printAlldata = () => {
-	document.body.append(`<div><p>${allData}</p></div>`)
+const theBigDevFunction = () => {
+	let containerFilter = document.createElement('DIV')
+	containerFilter.setAttribute('class', 'container')
+	containerFilter.setAttribute('id', 'filter-container')
+	document.body.appendChild(containerFilter)
+	let containerPodia = document.createElement('DIV')
+	containerPodia.setAttribute('class', 'container')
+	containerPodia.setAttribute('id', 'podia-container')
+	document.body.appendChild(containerPodia)
+	createHTMLElementsForEveryPodia()
+	createHTMLElementsForEveryFilter()
+}
+
+const createHTMLElementsForEveryPodia = () => {
+	for (const podium of allData) {
+		let element = document.createElement('DIV')
+		element.append(`${podium.name}`)
+		element.setAttribute('class', 'content-card')
+		element.setAttribute('id', podium.name)
+		document.getElementById('podia-container').appendChild(element)
+	}
+}
+const createHTMLElementsForEveryFilter = () => {
+
 }
