4d87899d068a69f95859962f7fd64f75b41e4467
docxjules <jules.docx@gmail.com>
2020-08-28 14:35:22 +0200

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var allData;
 
+fetch('/entries.json')
+  .then(res => res.json())
+  .then((json) => {
+	console.log(`Got the response, there are ${json.items.length} entries`)
+	allData = JSON.stringify(json.items);
+	printAlldata();
+});
+
+const printAlldata = () => {
+	document.body.append(`<div><p>${allData}</p></div>`)
+}
